03de915ac44308b87985ceab8ffd5f7792c98a9d
chas hornbaker <chas.hornbaker+1789674950@dbtlabsworkshop.com>
2026-09-17 21:03:38 +0000

    parity check

---
+++ analyses/order_profitability_parity.sql
@@ -0,0 +1,51 @@
+{% set legacy_query %}
+    select *
+    from DBT_LEARN.DBT_WORKSHOP_CHORNBAKER_A099FC.LEGACY_FCT_ORDER_PROFITABILITY
+{% endset %}
+
+{% set dbt_query %}
+    select *
+    from {{ ref('fct_order_profitability') }}
+{% endset %}
+
+{{
+    audit_helper.compare_and_classify_query_results(
+        a_query=legacy_query,
+        b_query=dbt_query,
+        primary_key_columns=['order_id'],
+        columns=[
+            'order_id',
+            'customer_id',
+            'customer_name',
+            'customer_email',
+            'home_region',
+            'favored_discipline',
+            'guild_id',
+            'guild_tier',
+            'shop_id',
+            'shop_name',
+            'shop_city',
+            'shop_region',
+            'ordered_at',
+            'ordered_date',
+            'order_status',
+            'order_channel',
+            'item_quantity',
+            'line_count',
+            'distinct_potions',
+            'gross_revenue_copper',
+            'gross_revenue_gold',
+            'discount_copper',
+            'discount_gold',
+            'net_revenue_copper',
+            'net_revenue_gold',
+            'paid_amount_copper',
+            'paid_amount_gold',
+            'latest_paid_at',
+            'payment_attempt_count',
+            'payment_state',
+            'is_home_region_order'
+        ],
+        sample_limit=20
+    )
+}}
